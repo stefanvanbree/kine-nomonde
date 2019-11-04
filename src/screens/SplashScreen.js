85fdc46b03ec1b61df879b0115f6c5205a170f10
stefanvanbree <stefan.schouteden@gmail.com>
2019-11-04 13:38:24 +0100

    renamed the file

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SplashScreen = () => {
+    return (
+        <View>
+            <Text>This is my Splash Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default SplashScreen;
+
